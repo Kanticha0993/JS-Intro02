672198276cabd636f7c15cb00c44ec5210d783d0
Kanticha0993 <s6606021610010@email.kmutnb.ac.th>
2024-12-03 09:35:05 +0700

    today

--- demovarletcunst.js
+++ demovarletcunst.js
@@ -7,8 +7,12 @@ console.log(hello);
 if(true){
     let World = "Hello World";
     console.log(World);
+    
 }
+//console.log(World)
 
 const fixval = "Fix Value";
 console.log(fixval);
+
+//fixval = "tets assingne value to const"
 
